c0109ddc5b574c415998b24be6e663d9b87ab540
Muhammad Zaeim Eusoff bin Mohd Ridzuan <148556849+zaeimeusoff@users.noreply.github.com>
2023-12-27 14:15:13 +0800

    Update program.py

--- program.py
+++ program.py
@@ -1,12 +1,4 @@
-import numpy as np
 import pandas as pd
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-from sklearn.neighbors import NearestNeighbors
-from sklearn.cluster import KMeans
-from sklearn.metrics import adjusted_rand_score
 
 datasetbefore = pd.read_csv("amazon.csv")
 dataset = pd.read_csv("amazon(cleaned).csv")
